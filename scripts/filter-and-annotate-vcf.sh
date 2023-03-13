04a2080fe525670bfd52455b80568e1947549ab6
kasiatom <katarzyna.tomala@uj.edu.pl>
2023-03-13 12:23:14 +0100

    filtered table

--- scripts/filter-and-annotate-vcf.sh
+++ scripts/filter-and-annotate-vcf.sh
@@ -66,3 +66,23 @@ mv $path/tmp5.vcf.gz $path/q_nq_annotated.vcf.gz
 mv $path/tmp5.vcf.gz.tbi $path/q_nq_annotated.vcf.gz.tbi
 
 rm $path/tmp*vcf.gz*    
+
+mkdir $path/out
+bcftools isec -C $path/q_nq_annotated.vcf.gz $path/q_nq_filtered-annotated.vcf.gz -p out
+bgzip -c $path/out/0000.vcf > $path/q_nq_removed-annotated.vcf.gz 
+tabix -p vcf $path/q_nq_removed-annotated.vcf.gz 
+rm -r $path/out
+
+## make table
+paste \
+<(printf "CHROM\tPOS\tREF\tALT\tQUAL\tVEP_Genes\tVEP_SYMBOLs\tVEP_IMPACTs\tVEP_Consequences\tVEP_BIOTYPEs\tVEP_Protein_positions\tVEP_Amino_acids\tVEP_WORST_Gene\tVEP_WORST_SYMBOL\tVEP_WORST_IMPACT\tVEP_WORST_Consequence") \
+<(bcftools view -h $path/q_nq_filtered-annotated.vcf.gz | tail -1 | cut -f10- | sed 's/\t/_GT\t/g' | sed 's/$/_GT/') \
+<(bcftools view -h $path/q_nq_filtered-annotated.vcf.gz | tail -1 | cut -f10- | sed 's/\t/_DP\t/g' | sed 's/$/_DP/') \
+<(bcftools view -h $path/q_nq_filtered-annotated.vcf.gz | tail -1 | cut -f10- | sed 's/\t/_AD\t/g' | sed 's/$/_AD/') > $path/header
+
+bcftools query -f "%CHROM\t%POS\t%REF\t%QUAL\t%INFO/VEP_Gene\t%INFO/VEP_SYMBOL\t%INFO/VEP_IMPACT\t%INFO/VEP_Consequence\t%INFO/VEP_BIOTYPE\t%INFO/VEP_Protein_position\t%INFO/VEP_Amino_acids\t%INFO/VEP_WORST_Gene\t%INFO/VEP_WORST_SYMBOL\t%INFO/VEP_WORST_IMPACT\t%INFO/VEP_WORST_Consequence[\t%GT][\t%DP][\t%AD]\n" $path/q_nq_filtered-annotated.vcf.gz \
+> $path/tmp.tsv
+cat $path/header $path/tmp.tsv > $path/q_nq.tsv
+
+rm $path/header $path/tmp.tsv
+cp $path/q_nq.tsv 
