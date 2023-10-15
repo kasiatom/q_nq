422317ffd70557231b08c20bda0627a58d99277c
kasiatom <katarzyna.tomala@uj.edu.pl>
2023-10-15 22:27:17 +0200

    some fixes

--- scripts/align.sh
+++ scripts/align.sh
@@ -3,9 +3,16 @@
 ## all programs are in the conda bio environment
 set -e -o  pipefail
 
-## find all sample names (here for raw fastq, but should be for the trimmed ones - after fastp with -r)
-samples=$( ls $HOME/ATAC_seq/fastqs/*gz | sed "s/-UJ.*//" |  sed 's|.*/||' | sort | uniq)
 
+## create output directory - if not present
+if [ ! -d $HOME/output ]
+then
+   mkdir $HOME/output
+fi   
+
+## find all sample names (here for raw fastq, but should be for the trimmed ones - after fastp with -r)
+#samples=$( ls $HOME/ATAC_seq/fastqs/*gz | sed "s/-UJ.*//" |  sed 's|.*/||' | sort | uniq)
+samples=127
 
 for sample in $samples
 do
@@ -15,23 +22,23 @@ do
    fastqs2=$(echo $fastqs1 | sed 's|_R1_|_R2_|g')
    ## align reads, reference is in /mnt/qnap/users/kasia.tomala/genome; here symlinked as $HOME/genome
    bowtie2 --very-sensitive-local \
-      -x $HOME/genome/R64-1-1 \  
-      -1 $fastqs11 -2 $fastqs2 -S "$sample".sam \
-      --rg-id "$sample" --rg "$sample" 2>"$sample"-alignment-stats.txt
+      -x $HOME/genome/R64-1-1 \
+      -1 $fastqs1 -2 $fastqs2 -S $HOME/output/"$sample".sam \
+      --rg-id "$sample" --rg "$sample" 2>$HOME/output/"$sample"-alignment-stats.txt
 
    ## makr duplicates
    gatk MarkDuplicates \
-      -I "$sample".sam \
-      -O "$sample"-markdup-unsorted.bam \
-      -M "$sample"-metrics.txt \
+      -I $HOME/output/"$sample".sam \
+      -O $HOME/output/"$sample"-markdup-unsorted.bam \
+      -M $HOME/output/"$sample"-metrics.txt \
       --ASSUME_SORT_ORDER  queryname
 
    ##sort and index bam
-   samtools sort "$sample"-markdup-unsorted.bam -@ 10 -o "$sample"-markdup.bam
-   samtools index "$sample"-markdup.bam
+   samtools sort $HOME/output/"$sample"-markdup-unsorted.bam -@ 10 -o $HOME/output/"$sample"-markdup.bam
+   samtools index $HOME/output/"$sample"-markdup.bam
 
    ## clean
-   rm "$sample"-markdup-unsorted.bam "$sample".sam 
+   rm $HOME/output/"$sample"-markdup-unsorted.bam $HOME/output/"$sample".sam 
 
 done 
 
